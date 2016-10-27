c0f1707c1d2a272cd929f6f439015a50fce9a75b
Yangyazheng <2017948437@qq.com>
2016-10-27 22:20:44 +0800

    每次调度缓存文件的处理

--- azkaban-common/src/main/java/azkaban/scheduler/ScheduleStatisticManager.java
+++ azkaban-common/src/main/java/azkaban/scheduler/ScheduleStatisticManager.java
@@ -26,100 +26,138 @@ import azkaban.utils.JSONUtils;
 
 /**
  * TODO: This needs to be fleshed out and made into a proper singleton.
+ *
+ * 每个scheduleId的缓存数据放在一个单独的文件中，
+ * 这些文件放在同一个目录下
+ * 文件命名为：scheduleId.cache
  */
 public class ScheduleStatisticManager {
-  public static final int STAT_NUMBERS = 10;
+    public static final int STAT_NUMBERS = 10;
 
-  private static HashMap<Integer, Object> cacheLock =
-      new HashMap<Integer, Object>();
-  private static File cacheDirectory;
+    private static HashMap<Integer, Object> cacheLock =
+            new HashMap<Integer, Object>();
+    private static File cacheDirectory;
 
-  public static void invalidateCache(int scheduleId, File cacheDir) {
-    setCacheFolder(cacheDir);
-    // This should be silent and not fail
-    try {
-      Object lock = getLock(scheduleId);
-      synchronized (lock) {
-        getCacheFile(scheduleId).delete();
-      }
-      unLock(scheduleId);
-    } catch (Exception e) {
-    }
-  }
-
-  public static void saveCache(int scheduleId, Map<String, Object> data) {
-    Object lock = getLock(scheduleId);
-    try {
-      synchronized (lock) {
-        File cache = getCacheFile(scheduleId);
-        cache.createNewFile();
-        OutputStream output = new FileOutputStream(cache);
+    /**
+     * 使缓存失效
+     * 将schedule对应的缓存文件删除
+     * @param scheduleId
+     * @param cacheDir
+     */
+    public static void invalidateCache(int scheduleId, File cacheDir) {
+        setCacheFolder(cacheDir);
+        // This should be silent and not fail
         try {
-          JSONUtils.toJSON(data, output, false);
-        } finally {
-          output.close();
+            Object lock = getLock(scheduleId);
+            synchronized (lock) {
+                getCacheFile(scheduleId).delete();
+            }
+            unLock(scheduleId);
+        } catch (Exception e) {
         }
-      }
-    } catch (Exception e) {
-      e.printStackTrace();
     }
-    unLock(scheduleId);
-  }
 
-  public static Map<String, Object> loadCache(int scheduleId) {
-    Object lock = getLock(scheduleId);
-    try {
-      synchronized (lock) {
-        File cache = getCacheFile(scheduleId);
-        if (cache.exists() && cache.isFile()) {
-          Object dataObj = JSONUtils.parseJSONFromFile(cache);
-          if (dataObj instanceof Map<?, ?>) {
-            @SuppressWarnings("unchecked")
-            Map<String, Object> data = (Map<String, Object>) dataObj;
-            return data;
-          }
+    /**
+     * 保存缓存，
+     * 每个schedule的缓存存放在单独的文件中
+     * @param scheduleId
+     * @param data
+     */
+    public static void saveCache(int scheduleId, Map<String, Object> data) {
+        Object lock = getLock(scheduleId);
+        try {
+            synchronized (lock) {
+                File cache = getCacheFile(scheduleId);
+                cache.createNewFile();
+                OutputStream output = new FileOutputStream(cache);
+                try {
+                    JSONUtils.toJSON(data, output, false);
+                } finally {
+                    output.close();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-      }
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-    unLock(scheduleId);
-    return null;
-  }
-
-  public static File getCacheDirectory() {
-    return cacheDirectory;
-  }
-
-  private static File getCacheFile(int scheduleId) {
-    cacheDirectory.mkdirs();
-    File file = new File(cacheDirectory, scheduleId + ".cache");
-    return file;
-  }
-
-  private static Object getLock(int scheduleId) {
-    Object lock = null;
-    synchronized (cacheLock) {
-      lock = cacheLock.get(scheduleId);
-      if (lock == null) {
-        lock = new Object();
-        cacheLock.put(scheduleId, lock);
-      }
+        unLock(scheduleId);
     }
 
-    return lock;
-  }
-
-  private static void unLock(int scheduleId) {
-    synchronized (cacheLock) {
-      cacheLock.remove(scheduleId);
+    /**
+     * 加载缓存，
+     *
+     * 加载指定schedule的缓存（从对应的单个文件中加载）
+     * @param scheduleId
+     * @return
+     */
+    public static Map<String, Object> loadCache(int scheduleId) {
+        Object lock = getLock(scheduleId);
+        try {
+            synchronized (lock) {
+                File cache = getCacheFile(scheduleId);
+                if (cache.exists() && cache.isFile()) {
+                    Object dataObj = JSONUtils.parseJSONFromFile(cache);
+                    if (dataObj instanceof Map<?, ?>) {
+                        @SuppressWarnings("unchecked")
+                        Map<String, Object> data = (Map<String, Object>) dataObj;
+                        return data;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        unLock(scheduleId);
+        return null;
     }
-  }
 
-  public static void setCacheFolder(File cacheDir) {
-    if (cacheDirectory == null) {
-      cacheDirectory = new File(cacheDir, "schedule-statistics");
+    /**
+     * 获取缓存目录
+     * @return File类型
+     */
+    public static File getCacheDirectory() {
+        return cacheDirectory;
     }
-  }
+
+    /**
+     * 根据schedule的id获取对应的缓存文件，
+     *
+     * 文件名称为对应的scheduleId.cache
+     * @param scheduleId
+     * @return
+     */
+    private static File getCacheFile(int scheduleId) {
+        cacheDirectory.mkdirs();
+        File file = new File(cacheDirectory, scheduleId + ".cache");
+        return file;
+    }
+
+    private static Object getLock(int scheduleId) {
+        Object lock = null;
+        synchronized (cacheLock) {
+            lock = cacheLock.get(scheduleId);
+            if (lock == null) {
+                lock = new Object();
+                cacheLock.put(scheduleId, lock);
+            }
+        }
+
+        return lock;
+    }
+
+    private static void unLock(int scheduleId) {
+        synchronized (cacheLock) {
+            cacheLock.remove(scheduleId);
+        }
+    }
+
+    /**
+     * 设置缓存目录
+     * @param cacheDir
+     */
+    public static void setCacheFolder(File cacheDir) {
+        if (cacheDirectory == null) {
+            cacheDirectory = new File(cacheDir, "schedule-statistics");
+        }
+    }
 }
 
